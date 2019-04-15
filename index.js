f866bd1e76a4870450851ddd715c244c6e7afc0c
Waleed <waleedkalyar48@gmail.com>
2019-04-15 15:44:56 +0500

    add env for ip and port

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ const server = net.createServer((socket)=>{
 	socket.pipe(socket);
 });
 
-const OPENSHIFT_NODEJS_PORT=8080;
-const OPENSHIFT_NODEJS_IP = '172.30.83.1';
+//const OPENSHIFT_NODEJS_PORT=8080;
+//const OPENSHIFT_NODEJS_IP = '172.30.83.1';
 
-var server_port = OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip = OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port =process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 server.listen(server_port,server_ip);
 
